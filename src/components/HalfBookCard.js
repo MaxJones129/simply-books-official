2f9a4c901b6de069355f905c86b452ca7164346d
MaxJones129 <maxjones129@gmail.com>
2024-10-05 12:29:56 -0500

    completed MVP, no stretch goals

--- src/components/HalfBookCard.js
+++ src/components/HalfBookCard.js
@@ -2,19 +2,14 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import Button from 'react-bootstrap/Button';
+// import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import Link from 'next/link';
-import { deleteBook } from '../api/bookData';
+// import Link from 'next/link';
+// import { deleteBook } from '../api/bookData';
 
-function BookCard({ bookObj, onUpdate }) {
+function HalfBookCard({ bookObj }) {
   // FOR DELETE, WE NEED TO REMOVE THE BOOK AND HAVE THE VIEW RERENDER,
   // SO WE PASS THE FUNCTION FROM THE PARENT THAT GETS THE BOOKS
-  const deleteThisBook = () => {
-    if (window.confirm(`Delete ${bookObj.title}?`)) {
-      deleteBook(bookObj.firebaseKey).then(() => onUpdate());
-    }
-  };
 
   return (
     <Card style={{ width: '18rem', margin: '10px' }}>
@@ -30,25 +25,12 @@ function BookCard({ bookObj, onUpdate }) {
           )}{' '}
           ${bookObj.price}
         </p>
-        {/* DYNAMIC LINK TO VIEW THE BOOK DETAILS  */}
-        <Link href={`/book/${bookObj.firebaseKey}`} passHref>
-          <Button variant="primary" className="m-2">
-            VIEW
-          </Button>
-        </Link>
-        {/* DYNAMIC LINK TO EDIT THE BOOK DETAILS  */}
-        <Link href={`/book/edit/${bookObj.firebaseKey}`} passHref>
-          <Button variant="info">EDIT</Button>
-        </Link>
-        <Button variant="danger" onClick={deleteThisBook} className="m-2">
-          DELETE
-        </Button>
       </Card.Body>
     </Card>
   );
 }
 
-BookCard.propTypes = {
+HalfBookCard.propTypes = {
   bookObj: PropTypes.shape({
     image: PropTypes.string,
     title: PropTypes.string,
@@ -56,7 +38,6 @@ BookCard.propTypes = {
     price: PropTypes.string,
     firebaseKey: PropTypes.string,
   }).isRequired,
-  onUpdate: PropTypes.func.isRequired,
 };
 
-export default BookCard;
+export default HalfBookCard;
